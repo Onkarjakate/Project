76c241c225763ba389d6bcfe83f1078e01da8ed2
Onkarjakate <101555275+Onkarjakate@users.noreply.github.com>
2022-04-07 23:48:22 -0700

    Add files via upload

--- GroupProject/src/test/java/test/NewTest.java
+++ GroupProject/src/test/java/test/NewTest.java
@@ -1,35 +1,122 @@
 package test;
-
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterTest;
-
+import test.MyWork;
 public class NewTest {
-	public static WebDriver driver;
-  @Test
-  public void f() {
-	  WebElement city = driver.findElement(By.xpath("//*[@id=\"modal-root\"]/div/div/div/div[1]/div/div/input"));
-	  city.sendKeys("mumbai");
-	  driver.findElement(By.xpath("//*[@id=\"modal-root\"]/div/div/div/div[2]/ul/li[1]/div/div/img")).click();
-	  driver.findElement(By.xpath("//*[@id=\"modal-root\"]/div/div/div/div[2]/ul/li[1]/div/div/img")).click();
-	  driver.findElement(By.xpath("//*[@id=\"wzrk-cancel\"]]")).click();
-	  WebElement rrr = driver.findElement(By.cssSelector("#super-container > div.sc-b1h692-2.bjlzfS > div.sc-133848s-2.sc-b1h692-4.gHrdsr > div.sc-b1h692-8.jfArFP > div:nth-child(2) > div > div > div > div.sc-lnhrs7-0.bKwTw > div > div.sc-lnhrs7-2.eQezya > div:nth-child(1) > a > div > div.sc-133848s-2.sc-1t5vwh0-0.kSxFV > div > img"));
-	  rrr.click();
-  }
-  @BeforeTest
-  public void beforeTest() {
-	  System.setProperty("webdriver.chrome.driver", "F:\\Drivers2\\chromedriver.exe");
-	  driver = new ChromeDriver();
-	  driver.get("http://www.bookmyshow.com");
-	  driver.manage().window().maximize();
-  }
+	
+		public static WebDriver driver;
+		public static Properties prop;
+		public static String sAppURL;
+		public static String city;
+		public static String movie;
+		public static Actions actions;
+		public static String readXL1;	
+		
+		
+		
+	  @SuppressWarnings("unused")
+	@Test
+	  public void f() throws InterruptedException,IOException{
+		  
+		  actions.moveToElement(driver.findElement(By.cssSelector("#super-container > div.sc-b1h692-2.bjlzfS > header > div.sc-gmeYpB.bFJFiZ > div > div > div > div.sc-eInJlc.fIvCmI > div:nth-child(1) > span"))).perform();	 
+		  
+		  String []data = MyWork.readXL1();
+		  driver.findElement(By.xpath("//*[@id=\'modal-root\']/div/div/div/div[2]/ul/li[1]/div/div/img")).sendKeys(data[1]);
+		  System.out.println('\n'+"Search Results for "+ data[1]);
+		  //driver.findElement(By.linkText("sc-bqjOQT aUKrX")).click();
+			//driver.findElement(By.xpath("//*[@id=\"super-container\"]/div[2]/div[4]/div[2]/div[4]/div/div/div[2]/a[1]/div/div[2]/div/img")).click();
+			//driver.findElement(By.xpath("//*[@id=\'modal-root\']/div/div/div/div[2]/ul/li[1]/div/div/img")).sendKeys(data[1]);
+		  //driver.findElement(By.xpath("//*[@id=\'modal-root\']/div/div/div/div[2]/ul/li[1]/div/div/img")).click();
+			driver.findElement(By.xpath("//*[@id=\'super-container\']/div[2]/div[2]/div[1]/div[2]/div/div/div/div[2]/div/div[1]/div[1]/a/div/div[3]/div[1]/div")).click();
+			 System.out.println('\n'+"Search Results for "+ data[1]);
+			//System.out.println('\n'+m);
+		  
+		  
+		  
+		/*for(int counter =1; counter<data.length;counter++)
+		 {
+			 actions.moveToElement(driver.findElement(By.xpath("//*[@id=\'super-container\']/div[2]/header/div[1]/div/div/div/div[2]/div[1]/span"))).perform();
+			 //driver.findElement(By.xpath("//*[@id=\'super-container\']/div[2]/header/div[1]/div/div/div/div[2]/div[1]/span")).click();//click on city
+			 driver.findElement(By.xpath("//*[@id=\'modal-root\"]/div/div/div/div[2]/ul/li[1]/div/div/img")).sendKeys(data[counter]);//city_name
+			 driver.findElement(By.xpath("//*[@id=\'super-container\']/div[2]/div[2]/div[1]/div[2]/div/div/div/div[2]/div/div[1]/div[1]/a/div/div[2]/div/img")).click();//rrrmovie
+			 Thread.sleep(5000);
+		 }*/
+		 
+		  
+		  String Header = driver.findElement(By.xpath("//*[@id='component-1']/section/span/h4")).getText();
+		  String aboutTheMovie = driver.findElement(By.xpath("//*[@id='component-1']/section/span/div/span")).getText();
 
-  @AfterTest
-  public void afterTest() {
-  }
+			 
+			 
+			 
+		 }
+			 
+		/*  driver.findElement(By.xpath("//*[@id=\'super-container\']/div[2]/header/div[1]/div/div/div/div[2]/div[1]/span")).click();
+		  WebElement city= driver.findElement(By.xpath("//*[@id=\'modal-root\']/div/div/div/div[2]/ul/li[1]/div/div/img"));
+		  city.sendKeys(sloc);
+		  WebElement movie=driver.findElement(By.xpath("//*[@id=\'super-container\']/div[2]/div[2]/div[1]/div[2]/div/div/div/div[2]/div/div[1]/div[1]/a/div/div[2]/div/img"));
+		  movie.sendKeys(sloc);
+		  driver.findElement(By.xpath("//*[@id=\"modal-root\"]/div/div/div/div[2]/ul/li[1]/div/div/img")).click();
+		  //WebElement m = driver.findElement(By.xpath("//*[@id=\"super-container\"]/div[2]/header/div[2]/div/div/div/div[1]/div/a[1]"));
+			//m.click();
+			//WebElement h = driver.findElement(By.xpath("//*[@id=\"super-container\"]/div[2]/header/div[2]/div/div/div/div[1]/div/a[1]"));
+			//h.click();
+			
+		  WebElement rrr = driver.findElement(By.xpath("//*[@id=\'super-container\']/div[2]/div[4]/div[2]/div[4]/div/div/div[2]/a[1]/div/div[2]/div/img"));
+		 // rrr.click();
+		  //WebElement l = driver.findElement(By.xpath("//*[@id=\"component-1\"]/section/span/h4"));
+		  
+		  
+		  
+		  		//WebElement rrr = driver.findElement(By.cssSelector("img[alt='RRR']"));
+			    //rrr.click();
+			  
+		  */
+	 
+	  @BeforeTest
+	  public void beforeTest() throws FileNotFoundException, IOException {
+		  try {
+			  prop = new Properties();
+		  }catch (Exception e) {
+			  
+			  e.printStackTrace();
+		  }
+		  
+		  prop.load(new FileInputStream("D:\\JavaWorkspace\\GroupAssignment\\Confirguration\\BO_MyShow.properties"));
+		  sAppURL = prop.getProperty("sAppURL");
+		  city = prop.getProperty("Mumbai");
+		  movie = prop.getProperty("RRR");
+		  
+		  
+		  System.setProperty("webdriver.chrome.driver","D:\\Drivers\\chromedriver_win32\\chromedriver.exe");
+		  ChromeOptions options = new ChromeOptions();
+		  options.addArguments("--disable-notifications");
+		  driver = new ChromeDriver(options);
+		  driver.get("http://www.bookmyshow.com");
+		  actions = new Actions(driver);
+		  driver.manage().window().maximize();
+		  driver.get(sAppURL);
+		  
+		  
+		  
+		    }
 
-}
+	  @AfterTest
+	  public void afterTest() {
+		  //driver.quit();
+	  }
+
+	}
